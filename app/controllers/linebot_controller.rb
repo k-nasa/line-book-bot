e8ab8edb78debd4ed1b32bd2d3f5d3bbfb702216
nasa <htilcs1115@gmail.com>
2017-12-06 11:18:56 +0900

    first commit

---
+++ app/controllers/linebot_controller.rb
@@ -0,0 +1,39 @@
+class LinebotController < ApplicationController
+  require 'line/bot'
+  protect_from_forgery :except => [:callback]
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless clinet.validate_signature(body,signature)
+      error 400 do 'Bad request' end
+    end
+
+    events = client.parse_events_from(body)
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          response = client.reply_message(event['replyToken'], message)
+          p response
+        end
+      end
+    }
+    head :ok
+  end
+
+
+  private 
+    def clinet 
+      @clinet ||= Line::Bot::Client.new{ |config| 
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        cinfig.channel_token = ENV["LINE_CHANNEL_TOLEN"]
+      }
+    end
+end
