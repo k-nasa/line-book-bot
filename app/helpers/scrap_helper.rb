722ec78e8aad3502f7e08f36b51587fd09603a80
nasa <htilcs1115@gmail.com>
2017-12-09 08:55:05 +0900

    リファクタリング

--- app/helpers/scrap_helper.rb
+++ app/helpers/scrap_helper.rb
@@ -11,7 +11,7 @@ module ScrapHelper
 
     doc = Nokogiri::HTML.parse(html)
 
-    day =  doc.xpath('//td[@class="products-td"]')[Date.today.day-1]
+    day =  doc.xpath('//td[@class="products-td"]')[Date.today.day-1-1]
     # day =  doc.xpath('//td[@class="products-td"]')[0]
     book_list = day.search("div.product-description-right a")
 
