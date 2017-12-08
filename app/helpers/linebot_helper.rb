a211069190bf906838d06b1d34577557220d601d
nasa <htilcs1115@gmail.com>
2017-12-08 19:39:34 +0900

    購読リストに追加

--- app/helpers/linebot_helper.rb
+++ app/helpers/linebot_helper.rb
@@ -31,11 +31,12 @@ module LinebotHelper
     postback_data = event['postback']['data'].split("\n")
     user = User.find_by_line_id(user_id)
     if user
-      record = user.SubscriptionList.find_by(record_type: type,content: postback_data[1])
-      record  = user.SubscriptionList.build(record_type: type,content: postback_data[1] ) unless record
-      if record.save
-        client.push_message(user_id,{type: "text",text: "#{postback_data[1]}を購読リストに保存しました(type#{type})"})
-      end 
+      unless user.SubscriptionList.find_by(record_type: type,content: postback_data[1])
+        record  = user.SubscriptionList.build(record_type: type,content: postback_data[1] ) 
+        if record.save
+          client.push_message(user_id,{type: "text",text: "#{postback_data[1]}を購読リストに保存しました(type#{type})"})
+        end 
+      end
     end
   end
 end
