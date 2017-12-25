da9896e3ce9be7e179ca7523c5a22d08dff5b236
nasa <htilcs1115@gmail.com>
2017-12-25 12:58:44 +0900

    前後３ヶ月分のデータを収集

--- app/helpers/scrap_helper.rb
+++ app/helpers/scrap_helper.rb
@@ -153,9 +153,9 @@ module ScrapHelper
       "https://calendar.gameiroiro.com/litenovel.php?year=#{(date >> -2).year}&month=#{(date >> -2).month}",
       "https://calendar.gameiroiro.com/manga.php",
       "https://calendar.gameiroiro.com/manga.php?year=#{(date >> 1).year}&month=#{(date >> 1).month}",
-      "https://calendar.gameiroiro.com/manga.php?year=#{(date >> 2).year}&month=#{(date >> 2).month}"
+      "https://calendar.gameiroiro.com/manga.php?year=#{(date >> 2).year}&month=#{(date >> 2).month}",
       "https://calendar.gameiroiro.com/manga.php?year=#{(date >> -1).year}&month=#{(date >> -1).month}",
-      "https://calendar.gameiroiro.com/manga.php?year=#{(date >> -2).year}&month=#{(date >> -2).month}"
+      "https://calendar.gameiroiro.com/manga.php?year=#{(date >> -2).year}&month=#{(date >> -2).month}",
     ]
 
   end
